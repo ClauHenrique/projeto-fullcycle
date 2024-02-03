d97bbb81d7038869965a0eab04b31c0d464422a8
ClauHenrique <johnhenriqueoliveira2021@gmail.com>
2024-02-03 11:49:04 -0300

    criar estrutura

---
+++ front-end/src/app/page1/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material";
+
+export default function Page1() {
+    return (
+        <div>
+            <p>Pagina 1</p>
+            <Button variant="contained" color="primary">teste</Button>
+        </div>
+    )
+}
