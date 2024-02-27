e4f1b340d79a0ed247c0629464f544b56a762e76
ClauHenrique <johnhenriqueoliveira2021@gmail.com>
2024-02-26 21:00:07 -0300

    conexao com rabbitmq

--- microsservico-pagamentos/go.mod
+++ microsservico-pagamentos/go.mod
@@ -1,3 +1,5 @@
 module github.com/ClauHenrique/projeto-fullcycle/microsservico-pagamentos
 
 go 1.21.6
+
+require github.com/rabbitmq/amqp091-go v1.9.0
--- microsservico-pagamentos/internal/entity/entity.go
+++ microsservico-pagamentos/internal/entity/entity.go
@@ -3,9 +3,17 @@ package entity
 import "errors"
 
 type OrderRequest struct {
-	OrderID  string  `json: "order_id"`
-	CardHash string  `json: "card_hash"`
+	OrderID  string  `json:"order_id"`
+	CardHash string  `json:"card_hash"`
 	Total    float64 `json:"total"`
+}
+
+func NewOrderRequest(orderID, cardHash string, total float64) *OrderRequest {
+	return &OrderRequest{
+		OrderID:  orderID,
+		CardHash: cardHash,
+		Total:    total,
+	}
 }
 
 func (order *OrderRequest) Validate() error {
@@ -22,4 +30,31 @@ func (order *OrderRequest) Validate() error {
 	}
 
 	return nil
+}
+
+type OrderResponse struct {
+	OrderID string `json:"order_id"`
+	Status  string `json:"status"`
+}
+
+func NewOrderResponse(orderID, status string) *OrderResponse {
+	return &OrderResponse{
+		OrderID: orderID,
+		Status:  status,
+	}
+}
+
+func (order *OrderRequest) Process() (*OrderResponse, error) {
+	err := order.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	orderResponse := NewOrderResponse(order.OrderID, "failed")
+
+	if order.Total < 100.00 {
+		orderResponse.Status = "paid"
+	}
+
+	return orderResponse, nil
 }
